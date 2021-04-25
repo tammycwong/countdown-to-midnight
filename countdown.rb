886f54fb65e2803dcaa9f9687ce4d457356cd2e6
Tammy <tammycwong@gmail.com>
2021-04-25 17:05:53 -0400

    finished

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    num = 10
+    while num >= 1 do 
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+    while num >= 0
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
